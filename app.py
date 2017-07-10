6757dcad279397107c2fcb79e8f0635febeaedd6
DavidNjoroge <njorogedavid13@gmail.com>
2017-07-10 13:11:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from  flask import Flask, render_template, request, json
 app=Flask(__name__)
 @app.route("/")
 def main():
-    return render_template('login.html')
+    return render_template('index.html')
 if __name__=="__main__":
     app.run(debug=True)
 
